cd5a46d134840fb6304f0fe7a3bb75420a27e0b9
杨伟诚 <yang.weicheng@xiaoyi.com>
2016-05-12 19:55:38 +0800

    Present UIImagePickerController

--- YWCVideoTrimmer/ViewController.swift
+++ YWCVideoTrimmer/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickVideo(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        let movieType: String = kUTTypeMovie as String
+        let vidoeType: String = kUTTypeVideo as String
+        picker.mediaTypes = [movieType,vidoeType]
+        self.presentViewController(picker, animated: true, completion: nil)
+        
+        
+        
+        
+        
+        
+    }
+    
 
 }
 
