f4800e439f4893352b4b3fa468004e9b1a619b9a
YangWeicheng <YangWeicheng@YangWeichengdeiMac.local>
2016-05-23 19:31:55 +0800

    Create cuttingViewController

---
+++ YWCVideoTrimmer/CuttingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CuttingViewController.swift
+//  YWCVideoTrimmer
+//
+//  Created by YangWeicheng on 5/23/16.
+//  Copyright © 2016 MI. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class CuttingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
